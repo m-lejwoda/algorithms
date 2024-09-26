654717906f3d58f1793b8242505c99d9d5801575
michal <michal.lejwoda@gmail.com>
2024-09-26 19:17:13 +0200

    przeszukiwanie_binarne

--- Algorytmika/rekurencja zadania/przeszukiwanie_binarne.py
+++ Algorytmika/rekurencja zadania/przeszukiwanie_binarne.py
@@ -1,14 +1,17 @@
 def przeszukiwanie_binarne(tab, x, l,r):
-    pass
-    if x == tab[r//2]:
-        print(r//2)
-        return x
-    if x > tab[r//2]:
-        return przeszukiwanie_binarne(tab, x, r//2, r)
-    if x < tab[r//2]:
-        return przeszukiwanie_binarne(tab, x, l, r//2)
+    if l > r:
+        return -1
+    else:
+        mid = (l + r) // 2
+        if x == tab[mid]:
+            return x
+        if x > tab[mid]:
+            return przeszukiwanie_binarne(tab, x, mid + 1, r)
+        else:
+            return przeszukiwanie_binarne(tab, x, l, mid - 1)
 
 
 
 tab = [1,2,6,18,20,23,29,32,34,39,40,49]
-przeszukiwanie_binarne(tab, 6,0, len(tab)-1)
+print(przeszukiwanie_binarne(tab, 6,0, len(tab)-1))
+print(przeszukiwanie_binarne(tab, 7,0, len(tab)-1))
